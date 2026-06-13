b180c3727756d2f1226563527434aa664602e000
Gabriel de Oliveira <gabab.code@gmail.com>
2026-06-12 22:48:05 -0300

    Commit

--- Manipu_Arquivos.c
+++ Manipu_Arquivos.c
@@ -3,13 +3,16 @@
 void main()
 {
     int a = 67;
+    char str[50];
     FILE *arquivo; //obrigatoria essa estrutura
 
-    arquivo = fopen("teste.txt", "w"); 
+    arquivo = fopen("teste.txt", "a+"); 
     
     if(arquivo != NULL){
-        printf("Arquivo aberto corretamente.");
-        fprintf(arquivo, "20+20+20+7, ai eh muito facil professora, eh %d", a);
+        //printf("Arquivo aberto corretamente.");
+        //fprintf(arquivo, "20+20+20+7, ai eh muito facil professora, eh %d", a);
+        fgets(str, 50, arquivo); //scanf ate achar um /n
+        printf("Lido do arquivo = %s", str);
         fclose(arquivo);
     } else {
         printf("Nao foi possivel abrir o arquivo");
